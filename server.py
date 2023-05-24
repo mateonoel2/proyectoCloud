e3aad768e170d1a8fce87d4f98bac62fe1665b7c
mateonoel2 <mateo.noel@utec.edu.pe>
2023-05-24 13:14:48 -0500

    Estado funcional

--- server.py
+++ server.py
@@ -1,13 +1,9 @@
 from flask import Flask, request, jsonify
-from pyspark.sql import SparkSession
-from pyspark.ml.feature import VectorAssembler
 import numpy as np
 from keras.models import load_model
+import pickle
 
 app = Flask(__name__)
-
-def is_short(input_data):
-    return 'TODO'
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -15,38 +11,22 @@ def predict():
     data = request.get_json(force=True)
     
     try:
-    
-        input_data = np.array([[data['day_of_week'], data['first_time'], data['total_distance'], data['first_stop'], data['target_stop']]])
+        # Cargar el modelo de red neuronal
+        features = np.array([[data['day_of_week'], data['first_time'], data['total_distance'], data['first_stop'], data['target_stop']]])
 
-        if(is_short(input_data)):
-            # Cargar el modelo de regresión lineal
+        # Load scaler
+        with open('models/rnn_scaler.pkl', 'rb') as file:
+            scaler = pickle.load(file)
+        scaled_features = scaler.transform(features)
 
-            spark = SparkSession.builder.appName('predict_short').getOrCreate()
+        scaled_features = np.reshape(scaled_features, (scaled_features.shape[0], 1, scaled_features.shape[1]))
+        scaled_features = [scaled_features]
 
-            input_data = spark.createDataFrame(input_data)
-            
-            assembler = VectorAssembler(inputCols=input_data.columns, outputCol="features_a")
-            features = assembler.transform(input_data)
+        print(scaled_features)
+        model = load_model('models/RNN_90.h5')
+        prediction = model.predict(scaled_features)
 
-            scaler = spark.read('trained_models/linearScaler').load('scaler')
-            scaled_features = scaler.transform(features)
-            
-            model = spark.read('trained_models/linearRegression').load('model')
-            prediction = model.transform(scaled_features)
-
-            return jsonify(prediction=prediction[0])
-        
-        else:
-            # Cargar el modelo de red neuronal
-            features = np.array([[data['day_of_week'], data['first_time'], data['total_distance'], data['first_stop'], data['target_stop']]])
-
-            scaler = load_model('/trained_models/RNNScaler')
-            scaled_features = scaler.transform(features)
-            
-            model = load_model('/trained_models/RNN_40.h5')
-            prediction = model.predict(scaled_features)
-
-            return jsonify(prediction=prediction[0])
+        return jsonify(prediction=prediction.tolist())
     
     except Exception as e:
         return jsonify(error=str(e))
